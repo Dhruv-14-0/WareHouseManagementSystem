86a0d86129ba37a6aa8f820a8ca226287a26847a
Dhruv Charkhawala <dv.charkhawala@gmail.com>
2025-08-21 23:14:38 +0530

    Initiazed project

---
+++ src/main/java/com/gatewaydigital/warewhousemanagementsystem/WarewhousemanagementsystemApplication.java
@@ -0,0 +1,13 @@
+package com.gatewaydigital.warewhousemanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarewhousemanagementsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WarewhousemanagementsystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gatewaydigital/warewhousemanagementsystem/WarewhousemanagementsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gatewaydigital.warewhousemanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarewhousemanagementsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
